802a0476b6ba709ce6c81f76ff4ec20b61e79faa
Jon Jagger <jon@jaggersoft.com>
2018-08-15 07:51:01 +0100

    drop running docker containers on TeamCity

--- sh/team_city_ci_pipe.sh
+++ sh/team_city_ci_pipe.sh
@@ -4,33 +4,12 @@ set -ex
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 source ${MY_DIR}/.env
 
-#one time cleanup
-docker rm --force faces-book-${FACES_BOOK_PORT}
-
 echo "Build the image"
 docker build \
   --build-arg REPO_URL=$(git config --get remote.origin.url) \
   --build-arg REPO_SHA=$(git rev-parse HEAD) \
   --tag ${FACES_BOOK_IMAGE} \
     ${MY_DIR}/../app
-
-
-echo "Bring down the current web-server if it exists"
-docker rm --force ${FACES_BOOK_CONTAINER} &> /dev/null || true
-
-echo "Bring up the new web-server"
-docker run \
-  --detach \
-  --name ${FACES_BOOK_CONTAINER} \
-  --publish ${FACES_BOOK_PORT}:${FACES_BOOK_PORT} \
-  --env FACES_BOOK_PORT=${FACES_BOOK_PORT} \
-    ${FACES_BOOK_IMAGE}
-
-echo "Crude wait for readyness"
-sleep 2
-
-echo "TODO: run some curl tests"
-#...
 
 echo "Tag the image"
 docker tag \
@@ -40,9 +19,6 @@ docker tag \
 echo "Push the image to the repository"
 docker push \
   ${DOCKER_REGISTRY_URL}/${FACES_BOOK_IMAGE}
-
-echo "Bring down the web-server"
-docker rm --force ${FACES_BOOK_CONTAINER} &> /dev/null || true
 
 echo "Remove the local images"
 docker rmi ${FACES_BOOK_IMAGE}
